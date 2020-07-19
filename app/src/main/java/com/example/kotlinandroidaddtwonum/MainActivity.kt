27e54fb3e5e0a9015f0483537bfe398e3c648610
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-19 03:16:00 +0100

    complete functionality to add two numbers

--- app/src/main/java/com/example/kotlinandroidaddtwonum/MainActivity.kt
+++ app/src/main/java/com/example/kotlinandroidaddtwonum/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlinandroidaddtwonum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View.VISIBLE
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAdd.setOnClickListener{
+            var num1Entered:Int=etNum1.text.toString().toInt()
+            var num2Entered:Int=etNum2.text.toString().toInt()
+            tvResult.visibility=VISIBLE
+            var result=sumNumbers(num1Entered,num2Entered)
+            tvResult.text = result.toString()
+        }
+    }
+
+    private fun sumNumbers(num1: Int, num2: Int): Int {
+        return num1 + num2
     }
 }
